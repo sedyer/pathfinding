1fa48ad40ebb7b86d4cedde3ebebba6022ae8ad0
Shane Dyer <Shane.Dyer@zirmed.com>
2017-06-05 14:26:39 -0400

    move everything into pathfinding.cpp

--- pathfinding/pathfinding/pathfinding.cpp
+++ pathfinding/pathfinding/pathfinding.cpp
@@ -1,5 +1,6 @@
-#include "stdafx.h"
-#include "pathfinding.h"
+#include <stdio.h>
+#include <tchar.h>
+#include <SDKDDKVer.h>
 #include <iostream>
 #include <algorithm>
 #include <list>  
@@ -16,6 +17,17 @@ using namespace std;
 class pathfinding {
 
 	mutex _lock;
+
+	template <typename K, typename V>
+	V getWithDefault(const  map <K, V> & m, const K & key, const V & defval) {
+		typename map<K, V>::const_iterator it = m.find(key);
+		if (it == m.end()) {
+			return defval;
+		}
+		else {
+			return it->second;
+		}
+	}
 
 	int getIndex(
 		int column,
@@ -284,3 +296,47 @@ public:
 
 	}
 };
+
+int main()
+{
+	pathfinding *pf = new pathfinding();
+
+	//given examples
+
+	unsigned char pMap[] = { 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1 };
+
+	int pOutBuffer[12];
+
+	int result = pf->FindPath(0, 0, 1, 2, pMap, 4, 3, pOutBuffer, 12);
+
+	/*unsigned char pMap[] = { 0, 0, 1, 0, 1, 1, 1, 0, 1 };
+
+	int pOutBuffer[7];
+
+	int result = pf->FindPath(2, 0, 0, 2, pMap, 3, 3, pOutBuffer, 7);*/
+
+	//thread safety demo
+
+	/*unsigned char pMap[] = {
+	1,1,1,0,1,1,
+	1,1,1,0,1,1,
+	1,0,1,1,1,1,
+	1,1,1,0,0,1,
+	1,1,1,1,0,1,
+	1,1,1,0,1,1
+	};
+
+	int pOutBuffer[24];
+
+	thread t1 = pf->pathfindingThread(4, 5, 0, 3, ref(pMap), 6, 6, ref(pOutBuffer), 24);
+	thread t2 = pf->pathfindingThread(0, 3, 4, 5, ref(pMap), 6, 6, ref(pOutBuffer), 24); //reversal of t1
+	thread t3 = pf->pathfindingThread(0, 3, 3, 5, ref(pMap), 6, 6, ref(pOutBuffer), 24); //no path, won't write to buffer/console
+	thread t4 = pf->pathfindingThread(2, 0, 2, 4, ref(pMap), 6, 6, ref(pOutBuffer), 24); //straight line down, 8 14 20 26
+
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();*/
+
+	return 0;
+}
